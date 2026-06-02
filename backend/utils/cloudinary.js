d00265cbcea59d75fd61a6f1c017fe73f3c105c9
Spurthi Alapati <spurthialapati2005@gmail.com>
2026-06-02 22:33:53 +0530

    Delete backend/utils/cloudinary.js

--- backend/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-console.log(process.env.API_KEY);
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
-export default cloudinary;
